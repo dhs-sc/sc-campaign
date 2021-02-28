d6ecabf646d2f58ba54e12848c9ae297989d639c
Joel Leo <leo.qiyi.joel@dhs.sg>
2021-02-28 21:44:38 +0800

    fix homepage link

--- assets/js/inject-base.js
+++ assets/js/inject-base.js
@@ -1,6 +1,6 @@
 const baseURL = window.location.origin;
 const pathName = window.location.pathname;
-const pathIndex = pathName.indexOf("/sc-campaign") + 12; // on the assumption that one has git clone from repo, and have no files named sc-campaign
+const pathIndex = pathName.lastIndexOf("/sc-campaign") + 12; // on the assumption that one has git clone from repo, and have no files named sc-campaign
 document.write(
 	`<base href='${
 		baseURL + pathName.substring(0, pathIndex) + "/"
