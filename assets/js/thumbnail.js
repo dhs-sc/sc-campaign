5a1fac4744df60f3199e5d87ad4c65edb9e6f64b
Joel Leo <leo.qiyi.joel@dhs.sg>
2021-04-08 01:47:33 +0800

    improve loading speed by using pagination and solve issue relating to chrome autoplay

--- assets/js/thumbnail.js
+++ assets/js/thumbnail.js
@@ -1,4 +1,4 @@
-function generateThumbnail(records, page, options) {
+function generateThumbnail(records, page, level, options) {
 	let html = "";
 	const path = "assets/thumbnails/";
 	for (let i = 0; i < records.length; i += 3) {
@@ -9,6 +9,12 @@ function generateThumbnail(records, page, options) {
 				const context = {
 					...records[j],
 					source: `${path}${thumbnail}`,
+					href:
+						page === "catalog"
+							? "#"
+							: `viewer.html?p=${page.toLowerCase()}&l=${level.toLowerCase()}&id=${records[
+									j
+							  ].id.toUpperCase()}`,
 				};
 				html = html + options.fn(context);
 			}
@@ -24,16 +30,16 @@ Handlebars.registerHelper("thumbnail", function (data, options) {
 	const page = url.searchParams.get("p");
 	if (page == "fpp") {
 		const groups = data.group[level];
-		return generateThumbnail(groups, page, options);
+		return generateThumbnail(groups, page, level, options);
 	} else {
 		const indiv = data.indiv[level];
-		return generateThumbnail(indiv, page, options);
+		return generateThumbnail(indiv, page, level, options);
 	}
 });
 
 $.getJSON("assets/json/student-data.json", (context) => {
 	var templateScript = Handlebars.compile(
-		`{{#thumbnail data}} <div class="col-4 mb-5" data-toggle="modal" data-target="#modal_{{id}}"> <h4 class="student-id">{{id}}</h4> <img src="{{source}}" class="student-img" alt="Error loading {{id}}'s photo here" /> <h4 class="student-name">{{name}}</h4> </div> {{/thumbnail}}`
+		`{{#thumbnail data}} <div class="col-4 mb-5"> <a href="{{href}}" class="thumbnail_link"> <h4 class="student-id">{{id}}</h4> <img src="{{source}}" class="student-img" alt="Error loading {{id}}'s photo here" /> <h4 class="student-name">{{name}}</h4> </a> </div> {{/thumbnail}} `
 	);
 
 	var html = templateScript(context);
---
+++ assets/js/viewer.js
@@ -0,0 +1,83 @@
+function getSourceType(source) {
+	let source_type = "";
+	const imageFileTypes = [".png", ".jpg"];
+	const videoFileTypes = [".mp4"];
+	imageFileTypes.forEach((type) => {
+		if (source.search(type) != -1) source_type = "image";
+	});
+	videoFileTypes.forEach((type) => {
+		if (source.search(type) != -1) source_type = "video";
+	});
+	if (source.search("http") != -1) return "external";
+	return source_type;
+}
+
+function generateViewer(records, page, cid, options) {
+	const path = "assets/modals/";
+	const record = records.find((r) => r.id === cid.toUpperCase());
+	const source = record[page];
+	const source_type = getSourceType(source);
+	const context = {
+		...record,
+		source_type,
+		source:
+			source_type == "video" || source_type == "image"
+				? `${path}${source}`
+				: source,
+	};
+	return options.fn(context);
+}
+
+Handlebars.registerHelper("viewer", function (data, options) {
+	const url = new URL(window.location.href);
+	const level = url.searchParams.get("l");
+	const page = url.searchParams.get("p");
+	const cid = url.searchParams.get("id");
+	if (page == "fpp") {
+		const groups = data.group[level];
+		return generateViewer(groups, page, cid, options);
+	}
+	if (page == "catalog") {
+		// if catalog, no modal is required
+		return "";
+	}
+	const indiv = data.indiv[level];
+	return generateViewer(indiv, page, cid, options);
+});
+
+Handlebars.registerHelper("isVideo", function (value) {
+	return value === "video";
+});
+
+Handlebars.registerHelper("isImage", function (value) {
+	return value === "image";
+});
+
+Handlebars.registerHelper("ifExternal", function (value) {
+	return value === "external";
+});
+
+$.getJSON("assets/json/student-data.json", (context) => {
+	var templateScript = Handlebars.compile(
+		`{{#viewer data}} <div class="jumbotron text-center"> <h1 id="titleHeading">{{id}} - {{name}}</h1> </div> <div class="embedded-container"> {{#if (isVideo source_type)}} <video class="embedded-content" controls src="{{source}}"></video> {{/if}} {{#if (ifExternal source_type)}} <iframe class="embedded-content" src="{{source}}"> </iframe> {{/if}} {{#if (isImage source_type)}} <img src="{{source}}" class="embedded-content" alt="Error loading {{id}}'s photo here" /> {{/if}} </div> {{#if description}} <h6 class="mt-3 text-center pl-3 pr-3">{{description}}</h6> {{/if}} {{/viewer}}`
+	);
+
+	try {
+		var html = templateScript(context);
+
+		$("#viewer").after(html);
+
+		const source = $(".embedded-content").attr("src");
+		console.log(source);
+		$.get(source).fail(() => {
+			$(".embedded-container").after(
+				`<h6 class="mt-3 text-center pl-3 pr-3">Note: The content has yet to be released!</h6>`
+			);
+			$(".embedded-container").remove();
+		});
+	} catch (e) {
+		$("#viewer").after(
+			`<div class="jumbotron text-center"> <h1 id="titleHeading">Oh no! Something went wrong! Please check your URL parameters!</h1> </div>`
+		);
+	}
+});
